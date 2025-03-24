e1c7c332766022ed658859b37b03d4b1547a7098
SomeoneSerge <else@someonex.net>
2025-03-24 23:08:42 +0000

    default.nix: init; package activation: vendor

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/flokli/sdgocat
 
-go 1.24.1
-
-require github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
+go 1.23.6
--- main.go
+++ main.go
@@ -5,20 +5,61 @@ import (
 	"io"
 	"log"
 	"net"
+	"os"
+	"strconv"
+	"strings"
 	"sync"
-
-	"github.com/coreos/go-systemd/activation"
+	"syscall"
 )
 
 var (
 	remoteAddr = flag.String("remote", "localhost:22", "The remote address and port to proxy to")
 )
 
-func main() {
-	listeners, err := activation.Listeners()
-	if err != nil {
-		log.Panicf("cannot get listeners: %s", err)
+// https://github.com/coreos/go-systemd/blob/7d375ecc2b092916968b5601f74cca28a8de45dd/activation/files_unix.go#L46-L69
+// https://github.com/coreos/go-systemd/blob/main/LICENSE
+func ActivationFiles() []*os.File {
+	const (
+		// SD_LISTEN_FDS_START
+		listenFdsStart = 3
+	)
+	pid, err := strconv.Atoi(os.Getenv("LISTEN_PID"))
+	if err != nil || pid != os.Getpid() {
+		return nil
 	}
+	nfds, err := strconv.Atoi(os.Getenv("LISTEN_FDS"))
+	if err != nil || nfds == 0 {
+		return nil
+	}
+	names := strings.Split(os.Getenv("LISTEN_FDNAMES"), ":")
+	files := make([]*os.File, 0, nfds)
+	for fd := listenFdsStart; fd < listenFdsStart+nfds; fd++ {
+		syscall.CloseOnExec(fd)
+		name := "LISTEN_FD_" + strconv.Itoa(fd)
+		offset := fd - listenFdsStart
+		if offset < len(names) && len(names[offset]) > 0 {
+			name = names[offset]
+		}
+		files = append(files, os.NewFile(uintptr(fd), name))
+	}
+	return files
+}
+
+// https://github.com/coreos/go-systemd/blob/7d375ecc2b092916968b5601f74cca28a8de45dd/activation/listeners.go#L28-L39
+func ActivationListeners() []net.Listener {
+	files := ActivationFiles()
+	listeners := make([]net.Listener, len(files))
+	for i, f := range files {
+		if pc, err := net.FileListener(f); err == nil {
+			listeners[i] = pc
+			f.Close()
+		}
+	}
+	return listeners
+}
+
+func main() {
+	listeners := ActivationListeners()
 
 	if len(listeners) != 1 {
 		log.Panicf("unexpected number of listeners, got %d, expected 1", len(listeners))
